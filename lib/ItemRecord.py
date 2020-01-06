c41d86601f90f91f7ff48450acaa54bbe3f04084
dOtOlb <dOtOlb@gmail.com>
2020-01-06 08:14:20 +0800

    adding in lib components/functions

---
+++ lib/ItemRecord.py
@@ -0,0 +1,39 @@
+import sys
+import datetime
+import operator
+
+class ItemRecord:
+    """A data structure holding all the information about an item"""
+    def __init__(self, type, id, score, data):
+        self.type = type
+        self.id = id
+        self.score = score
+        self.boosted_score = score
+        self.data = data
+        self.time = datetime.datetime.now()
+        self.active = True
+
+    def get_id(self):
+        return self.id
+
+    def get_time(self):
+        return self.time
+
+    def get_score(self):
+        return self.score
+
+    def get_type(self):
+        return self.type
+
+    def get_data(self):
+        return self.data
+
+    def is_deleted(self):
+        return not self.active
+
+    def delete(self):
+        self.active = False
+
+    def boost_score(self, boost, start_over):
+        self.boosted_score = (self.score if start_over
+                                else self.boosted_score) * boost
