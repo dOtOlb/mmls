2b69fe500e931dea44b85bacc9088a1a7099181c
dOtOlb <dOtOlb@gmail.com>
2020-01-06 08:21:49 +0800

    added the example module for kick-off

---
+++ mods/SampleMod.py
@@ -0,0 +1,28 @@
+import sys
+import datetime
+import operator
+sys.path.append("../lib/")
+import Store
+
+class SampleMod:
+    """ a sample module implementation which presumes log-based pattern extraction """
+    __init__(self):
+        self.store = Store()
+
+    __input_from_file(fh):
+        count_commands = 0
+        for line in fh:
+            count_commands = count_commands or (int(line) + 1)
+
+            # extract into the internal store first
+            self.store.add(line)
+
+            count_commands -= 1
+            if (count_commands == 0): break
+
+    __run():
+        pass
+
+    __input_from_file = input_from_file
+
+    __run = run
